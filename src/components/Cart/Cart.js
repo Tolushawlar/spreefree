ac715f9685fddf16e2787ac8de910c8db0065270
Olusola i.t <Holushawlar.it@gmial.com>
2022-09-28 12:24:16 +0100

    cart fix

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -28,10 +28,10 @@ const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart }) => {
         ))}
       </Grid>
       <div className={classes.cardDetails}>
-      <Typography variant="h5" >Subtotal: <b >{cart.subtotal.formatted_with_symbol}</b></Typography>
+      {/*<Typography variant="h5" >Subtotal: <b >{cart.subtotal.formatted_with_symbol}</b></Typography>*/}
         <div>
           <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>Empty cart</Button>
-          <Button className={classes.checkoutButton} component={Link} to="/checkout" size="large" type="button" variant="contained" >Checkout</Button>
+          {/*<Button className={classes.checkoutButton} component={Link} to="/checkout" size="large" type="button" variant="contained" >Checkout</Button>*/}
         </div>
       </div>
     </>
--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -15,8 +15,10 @@ const CartItem = ({ item, onUpdateCartQty, onRemoveFromCart }) => {
       <CardMedia image={item.image.url} alt={item.name} className={classes.media} />
       <CardContent className={classes.cardContent}>
         <Typography variant="h6">{item.name}</Typography>
-        <Typography variant="h6" color='secondary' >{item.line_total.formatted_with_symbol}</Typography>
+        {/*<Typography variant="h6" color='secondary' >{item.line_total.formatted_with_symbol}</Typography>*/}
+      
       </CardContent>
+      {/*}
       <CardActions className={classes.cardActions}>
         <div className={classes.buttons}>
           <Button type="button" size="small" onClick={() => handleUpdateCartQty(item.id, item.quantity - 1)}>-</Button>
@@ -25,6 +27,7 @@ const CartItem = ({ item, onUpdateCartQty, onRemoveFromCart }) => {
         </div>
         <Button className={classes.button} variant="contained" type="button" color='secondary' onClick={() => handleRemoveFromCart(item.id)}>Remove</Button>
       </CardActions>
+      */}
     </Card>
   );
 };
