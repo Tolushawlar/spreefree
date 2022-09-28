ec8e9f28263e3c51f6bfa953ca84adc0a8a0a967
Olusola i.t <Holushawlar.it@gmial.com>
2022-09-28 12:28:07 +0100

    cart fix typo

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -30,7 +30,7 @@ const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart }) => {
       <div className={classes.cardDetails}>
       {/*<Typography variant="h5" >Subtotal: <b >{cart.subtotal.formatted_with_symbol}</b></Typography>*/}
         <div>
-          <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>Empty cart</Button>
+          <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>Empty Spree List</Button>
           {/*<Button className={classes.checkoutButton} component={Link} to="/checkout" size="large" type="button" variant="contained" >Checkout</Button>*/}
         </div>
       </div>
@@ -40,7 +40,7 @@ const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart }) => {
   return (
     <Container>
       <div className={classes.toolbar} />
-      <Typography className={classes.title} variant="h5" gutterBottom><b>Your Shopping Cart</b></Typography>
+      <Typography className={classes.title} variant="h5" gutterBottom><b>Your Spree List</b></Typography>
       <hr/>
       { !cart.line_items.length ? renderEmptyCart() : renderCart() }
     </Container>
